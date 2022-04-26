1d22fac0345a6e6ee689235e035ff265597618ef
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-04-26 12:38:29 +0600

    PHP-Day1 Installation

---
+++ class1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Class 1</title>
+</head>
+<body>
+    <?php
+        echo "Hello Akash. Welcome to PHP";
+    ?>
+</body>
+</html>
