e62ffb08a599836b3839e617a462bcbc8a8d76c5
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-05-07 20:54:05 +0600

    PHP Class - Day 3 - echo, commenting, print, concat

---
+++ class3.php
@@ -0,0 +1,62 @@
+<?php
+
+// echo "Welcome to Learn PHP";
+
+// $first_name = "Akash";
+
+// echo $first_name;
+
+// print "Welcome to Learn PHP";
+
+// $number1 = 10;
+// $number2 = 20;
+// echo $number1; // 10
+// echo $number2; // 20
+// echo $number1 + $number2; // 30
+
+// print $number1; // 10
+// print $number2; // 20
+// print $number1 + $number2; // 30
+
+// $name = "Jhon";
+// $age = 40;
+// $welcome_text = "Hello " . $name . ", You are " . $age . " years old.";
+// echo "Hello ";
+// echo $name;
+
+// echo $welcome_text; //Hello Jhon, You are 28 years old.
+
+$number1 = 10;
+$number2 = 20;
+
+$summation = $number1 + $number2;
+$subtraction = $number2 - $number1;
+
+// echo "<strong>Summation</strong> = " . $summation . "<br>";
+// echo "<strong>Subtraction</strong> = " . $subtraction;
+
+// var_dump(print(300));
+
+// Let's print Welcome to learn PHP in browser
+// print "Welcome to learn PHP";
+
+$number1 = 10; // Assign number1 to 10
+$number2 = 20; // Assign number2 to 20
+
+/*
+Let's make summation of
+number1 and number2
+and print it
+*/
+
+/**
+ * Let's make summation of
+ * number1 and number2
+ * and print it
+ */
+
+// $summation = $number1 + $number2;
+// echo "Summation = " . $summation;
+
+
+echo 'I\'m Maniruzzaman Akash';
