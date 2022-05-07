b6d27d075297ff69a371570dd3aab54b4b28cfaa
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-05-07 17:16:47 +0600

    PHP-class-day2- Variable and Variable types

---
+++ class2.php
@@ -0,0 +1,52 @@
+<?php
+
+// $name = "Akash";
+// $age = 28;
+// $AGE = 50;
+// $price = 50000.90;
+
+// $first_name = "Mani";
+// $last_name = "Akash";
+// $is_akash = true;
+
+
+// $firstname = "Mani";
+// $first_name = "Mani";
+// $firstName = "Mani";
+
+// $123 = 12;
+
+// echo $age;
+
+// echo $AGE;
+
+
+// $age = 2000;
+// var_dump($age); //int
+
+
+// $age = 2000.50;
+// var_dump($age); //float
+
+// $is_logged_in = false; // 0
+// // $is_logged_in = true; // 1
+// var_dump($is_logged_in); //bool
+
+// $first_name = 'Maniruzzaman';
+// $last_name = "Akash";
+// var_dump($first_name); //string(12)
+
+// $number1 = 10;
+// $number2 = 20;
+// var_dump($number1 + $number2);
+
+
+// $avatar_url = null;
+// var_dump($avatar_url);
+
+// car =>  oil = 2.5
+//         wheel = 2
+//         brand = "Tesla"
+
+$marks = [80.5, 90, 100];
+var_dump($marks);
